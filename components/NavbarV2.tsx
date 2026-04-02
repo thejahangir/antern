a8c2f4cd98f0fc8af5c892c98e9d2f65a6c05098
thejahangir <jahangirxl@gmail.com>
2026-04-02 15:40:04 +0530

    linkedin link updated

--- components/NavbarV2.tsx
+++ components/NavbarV2.tsx
@@ -195,7 +195,7 @@ export const NavbarV2: React.FC<NavbarProps> = ({
             </a>
           </div>
           <div className="flex items-center gap-4">
-            <a href="#" className="hover:text-[#0085F7] transition-colors">
+            <a href="https://www.linkedin.com/company/antern-technologies/" className="hover:text-[#0085F7] transition-colors" target="_blank">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
                 <rect x="2" y="9" width="4" height="12"></rect>
